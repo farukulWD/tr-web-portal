96088103af1844e680c36b88facea3fce4d44867
Sagor Ahamed <104268039+SagorAhamed251245@users.noreply.github.com>
2024-10-30 01:50:47 +0600

    updated

--- src/app/(auth)/page.tsx
+++ src/app/(auth)/page.tsx
@@ -1,16 +1,17 @@
 "use server";
-import { getUser } from "../api/userApi";
+import LoginComp from "@/components/login/LoginComp";
+// import { getUser } from "../../api/userApi";
 
-import { redirect } from "next/navigation";
+// import { redirect } from "next/navigation";
 
 export default async function HomePage() {
-  const user = await getUser();
-  if (!user) {
-    redirect("/login");
-  }
+  // const user = await getUser();
+  // if (!user) {
+  //   redirect("/login");
+  // }
   return (
-    <div className="flex w-full justify-center items-center h-screen">
-      Home page
-    </div>
+    <main className="flex w-full justify-center items-center h-screen">
+      <LoginComp />
+    </main>
   );
 }
