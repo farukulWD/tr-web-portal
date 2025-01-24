a4c7fc7eb44feb599fdc2e9b72a9ede432a02bdb
farukulWD <farukgb1999@gmail.com>
2025-01-24 23:10:36 +0600

    Add delete functionality for products in order table with loading state indication

--- src/components/Dashboard/Dealer/order-entry/OrderTable.tsx
+++ src/components/Dashboard/Dealer/order-entry/OrderTable.tsx
@@ -10,6 +10,9 @@ import {
 import GlobalTable from "@/components/shared/global/GlobalTable";
 import { IProduct, TDealerOrder } from "@/types";
 import dayjs from "dayjs";
+import { useDeleteProductFromOrderMutation } from "@/redux/api/orderApi/orderApi";
+import { globalErrorHandler } from "@/utils/globalErrorHandler";
+import { toast } from "sonner";
 
 interface ColumnConfig {
   key: string;
@@ -18,73 +21,93 @@ interface ColumnConfig {
   width?: string;
   render?: (value: any, item?: IProduct | undefined) => React.ReactNode;
 }
-const columns: ColumnConfig[] = [
-  {
-    key: "createdAt",
-    label: "Date",
-    align: "left",
-    render: (_, item) => {
-      return <p>{dayjs(item?.product?.createdAt).format("DD/MM/YYYY")}</p>;
-    },
-  },
-
-  {
-    key: "code",
-    label: "Code",
-    align: "center",
-    render: (_, item) => {
-      return <p>{item?.product?.productCode}</p>;
-    },
-  },
-  {
-    key: "name",
-    label: "Name",
-    align: "center",
-    width: "200px",
-    render: (_, item) => {
-      return <p>{item?.product?.name}</p>;
-    },
-  },
-  {
-    key: "price",
-    label: "Price",
-    align: "center",
-  },
-  // { key: "factor", label: "Factor", align: "center" },
-  { key: "quantity", label: "Qty", align: "center" },
-  {
-    key: "total",
-    label: "Total",
-    align: "center",
-    render: (_, item) => {
-      return (
-        <p>{(item?.product?.price as number) * (item?.quantity as number)}</p>
-      );
-    },
-  },
-  {
-    key: "action",
-    label: "Action",
-    align: "center",
-    render: (value: any, item: any | undefined) => (
-      <Tooltip>
-        <TooltipTrigger asChild>
-          <Button
-            variant="destructive"
-            size="sm"
-            // onClick={() => handleDelete(item.id)}
-          >
-            <Trash2 className="h-4 w-4" />
-            <span className="sr-only">Delete row {item?.id}</span>
-          </Button>
-        </TooltipTrigger>
-        <TooltipContent>Delete this item</TooltipContent>
-      </Tooltip>
-    ),
-  },
-];
 
 const OrderTable = ({ orderData }: { orderData: TDealerOrder }) => {
+  const [deleteFromOrder,{isLoading}] = useDeleteProductFromOrderMutation();
+
+  const hendleDelete = async (productId: string) => {
+    try {
+      const res = await deleteFromOrder({
+        orderId: orderData?._id as string,
+        productId,
+      }).unwrap();
+
+      if (res?.success) {
+        toast.success(res?.message);
+      }
+    } catch (error) {
+      globalErrorHandler(error);
+    }
+  };
+
+  const columns: ColumnConfig[] = [
+    {
+      key: "createdAt",
+      label: "Date",
+      align: "left",
+      render: (_, item) => {
+        return <p>{dayjs(item?.product?.createdAt).format("DD/MM/YYYY")}</p>;
+      },
+    },
+
+    {
+      key: "code",
+      label: "Code",
+      align: "center",
+      render: (_, item) => {
+        return <p>{item?.product?.productCode}</p>;
+      },
+    },
+    {
+      key: "name",
+      label: "Name",
+      align: "center",
+      width: "200px",
+      render: (_, item) => {
+        return <p>{item?.product?.name}</p>;
+      },
+    },
+    {
+      key: "price",
+      label: "Price",
+      align: "center",
+    },
+    // { key: "factor", label: "Factor", align: "center" },
+    { key: "quantity", label: "Qty", align: "center" },
+    {
+      key: "total",
+      label: "Total",
+      align: "center",
+      render: (_, item) => {
+        return (
+          <p>{(item?.product?.price as number) * (item?.quantity as number)}</p>
+        );
+      },
+    },
+    {
+      key: "action",
+      label: "Action",
+      align: "center",
+      render: (value: any, item: any | undefined) => {
+        return (
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Button
+              disabled={isLoading}
+                variant="destructive"
+                size="sm"
+                onClick={() => hendleDelete(item?._id)}
+              >
+                <Trash2  className={`h-4 w-4 ${isLoading&& "animate-spin"}`} />
+              </Button>
+            </TooltipTrigger>
+            <TooltipContent>Delete this item</TooltipContent>
+          </Tooltip>
+        );
+      },
+    },
+  ];
+
   const footers = [
     {
       key: "createdAt",
